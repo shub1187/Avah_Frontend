b5abf1078d9b2d1b5d283c4f33d4a30b3c6dc1ad
shub1187 <shubham.mohite1187@gmail.com>
2023-08-25 01:49:38 +0530

    Fixed Deny Button issue was not working

--- src/pages/dashboard/RequestsLayout.js
+++ src/pages/dashboard/RequestsLayout.js
@@ -84,7 +84,7 @@ const RequestsLayout = (props) => {
             <Button variant="outlined"   onClick={() => handleApprove(row)} color="success">
               APPROVE
             </Button>
-            <Button variant="outlined" onClick={() => handleDeny(row.id)} color="error">
+            <Button variant="outlined" onClick={() => handleDeny(row)} color="error">
               REJECT
             </Button>
           </Box>
