27a84eab577df786aa123eed9697669fed4b4686
Raees <raeesmohamed66@gmail.com>
2023-10-01 17:03:30 +0530

    d

--- src/pages/Customer/UpdateProfile/UpdateProfile.jsx
+++ src/pages/Customer/UpdateProfile/UpdateProfile.jsx
@@ -17,7 +17,7 @@ const UpdateCustomerProfile = ({height,width,color,minHeight,maxWidth,img,border
     const {fetchData} = useFetchFunction()
     const {fetchCustomerData,snackbar,loadingIndicator} = useCustomerFetchFunction()
     const {isMobile} = useMobileResponsive()
-    let {data:profileData} = useFetch('http://localhost:3008/api/customer/getAllCitiesPerState')
+    let {data:cityData} = useFetch('http://localhost:3008/api/customer/getAllCitiesPerState')
     const [data,setData] = React.useState([]) 
     const result= [
       {
@@ -38,7 +38,7 @@ const UpdateCustomerProfile = ({height,width,color,minHeight,maxWidth,img,border
           ]
       }
   ]
-  const selectArray = profileData?.result?.map((brandEntry) => {
+  const selectArray = cityData?.result?.map((brandEntry) => {
     const brandName = Object.keys(brandEntry)[0]; // Get the brand name
     const formattedBrandValue = brandName.toLowerCase().replace(/ /g, '_'); // Format the value
   
@@ -80,7 +80,7 @@ if (selectedBrand) {
       },[])
       console.log("ln 30 UpdateProfile.jsx data: ", data)
 
-      // let profileData = data?.result
+      let profileData = data?.result
 
     const [status,setStatus] = useState({
       isVisible:false,
@@ -188,7 +188,7 @@ if (selectedBrand) {
                 <Typography my={1} fontWeight={'bold'} fontSize={20}>Email : <Typography fontSize={20} sx={{color:'rgb(173,73,112)'}} fontWeight={'bold'} component={'span'}>{profileData?.email??""}</Typography></Typography>
               </Box>
               <Box mx={5}>
-                <Typography  my={1} fontWeight={'bold'} fontSize={20}>Address : <Typography fontSize={20} sx={{color:'rgb(173,73,112)'}} fontWeight={'bold'}  component={'span'}>{profileData?.address??""}</Typography></Typography>
+                <Typography  my={1} fontWeight={'bold'} fontSize={20}>Full Address : <Typography fontSize={20} sx={{color:'rgb(173,73,112)'}} fontWeight={'bold'}  component={'span'}>{profileData?.address??""}</Typography></Typography>
                 <Typography my={1} fontWeight={'bold'} fontSize={20}>Mobile Number : <Typography fontSize={20} sx={{color:'rgb(173,73,112)'}} fontWeight={'bold'} component={'span'}>{profileData?.mobile_number??""}</Typography></Typography>
               </Box>
             </Box>
