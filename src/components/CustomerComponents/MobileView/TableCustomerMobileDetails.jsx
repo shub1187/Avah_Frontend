0f10f3fd64ffc942d5b4f316db34c5ca660ed99f
Raees <raeesmohamed66@gmail.com>
2023-10-24 16:15:26 +0530

    d

--- src/components/CustomerComponents/MobileView/TableCustomerMobileDetails.jsx
+++ src/components/CustomerComponents/MobileView/TableCustomerMobileDetails.jsx
@@ -6,7 +6,7 @@ const TableCustomerMobileDetails = ({URL}) => {
     const {fetchCustomerData} = useCustomerFetchFunction()
     const [data,setData]= useState([])
     useEffect(()=>{
-        const {data} =fetchCustomerData({url:URL})
+        const {data} =fetchCustomerData({url:URL,method:'GET'})
         setData(data)
     }
     ,[])
