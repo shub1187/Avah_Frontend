c14be63179f446973f3b08aeda9c2a2a208f378a
shub1187 <shubham.mohite1187@gmail.com>
2023-11-28 12:14:53 +0530

    R&S integration

--- src/components/spComponents/Table/Columns/Labours/SpLabourColumn.jsx
+++ src/components/spComponents/Table/Columns/Labours/SpLabourColumn.jsx
@@ -2,5 +2,5 @@ export const spLabourColumns = [
     { title: "Item Description", field: "labour_name" },
     { title: "HSN/SAC", field: "hsn_sac" },
     { title: "Price", field: "price" },
-    { title: "Tax", field: "tax" },
+    { title: "Tax %", field: "tax" },
 ]
--- src/components/spComponents/Table/Columns/Spares/SpSparesColumn.jsx
+++ src/components/spComponents/Table/Columns/Spares/SpSparesColumn.jsx
@@ -6,10 +6,10 @@ export const SpcreateSparesColumn = [
     { title: "HSN/SAC", field: "hsn_sac" },
     { title: "Part Number", field: "part_number" },
     { title:'Selling Price', field : 'selling_price'},
-    { title: "Tax", field: "tax" },
-    { title :'Action', render : (rowData)=>{
+    { title: "Tax %", field: "tax" },
+    { title :'Action', render : (rowData)=>
         <Box display='flex'>
             <MoreActionDialog rowData={rowData}/>
         </Box>
-    }}
+    }
 ]
