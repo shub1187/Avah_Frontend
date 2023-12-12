4cb7958a8e03041883df071b1859c2e5e68cad48
Raees <raeesmohamed66@gmail.com>
2023-12-12 20:44:15 +0530

    d

--- src/components/spComponents/Table/Columns/Service/SpCreateLabourEstimateColumn.jsx
+++ src/components/spComponents/Table/Columns/Service/SpCreateLabourEstimateColumn.jsx
@@ -1,5 +1,5 @@
 export const SpCreateLabourEstimateColumn = [
-    { title: "Spare Description", field: "labour_name" },
+    { title: "Labour Description", field: "labour_name" },
     { title: "HSN/SAC", field: "hsn_sac" },
     { title: "Price", field: "selling_price" },
     { title: "Tax %", field: "tax" },
