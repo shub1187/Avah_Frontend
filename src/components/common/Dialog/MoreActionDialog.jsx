8b1a1c4b81ec1e6532749cf21704dda2f93c4ff7
Raees <raeesmohamed66@gmail.com>
2024-07-21 12:36:12 +0530

    f

--- src/components/common/Dialog/MoreActionDialog.jsx
+++ src/components/common/Dialog/MoreActionDialog.jsx
@@ -34,8 +34,8 @@ const MoreActionDialog = ({rowData,ActionDialog}) => {
                         if(typeof rowData[key] ==='string'){
                             return <Box width={'200px'} m={1}><InputLabel>{key.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</InputLabel><TextField size='small' multiline={key=='pickup_address' || key =='sp_rejection_note'?true:true} rows={key=='pickup_address' || key =='sp_rejection_note'  ? 3:1} fullWidth  value={rowData[key]} disabled/></Box>
                         }
-                        else if(Array.isArray(rowData[key])){
-                            return (<Box width={rowData?.permission_granted?'210px': '420px'} m={1}>
+                        else if(Array.isArray(rowData[key]) && !rowData?.permission_granted){
+                            return (<Box width={'420px'} m={1}>
                             <InputLabel>{key.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</InputLabel>
                             <Autocomplete
                                 value={rowData[key] || []}
